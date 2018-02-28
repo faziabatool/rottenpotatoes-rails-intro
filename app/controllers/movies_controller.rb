4b8833bcfd18962f3f9e3c5436fd46b62c19613c
Fazia Batool <faziabatool28@gmail.com>
2018-02-28 22:55:30 +0000

    homework-2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
-class MoviesController < ApplicationController
+
+  class MoviesController < ApplicationController
+
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
@@ -10,12 +12,48 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    @movies = Movie.all
+  def index 
+
+    # get the param or session value for the sort type
+    # its value is either 'sortByTitle' or 'sortByReleaseDate'
+    sorting_by = params[:sort_by] || session[:sort_by]
+    if sorting_by == 'sortByTitle'
+      order_by= (:title)
+    elsif sorting_by == 'sortByReleaseDate'
+      order_by = (:release_date)
+    end
+
+    # set the session 
+    session[:sort_by] = sorting_by
+
+    # getting all the values of rating from the class Movie.rd
+    # to display on the screen (@all_ratings being used in index.html)
+    @all_ratings = Movie.ratings
+
+    # get the param or session value for the ratings type
+    @ratings = params[:ratings] || session[:ratings] || nil
+
+    if @ratings == nil
+      @ratings = {"G" => "1", "PG" => "1", "PG-13" => "1", "R" => "1"}
+    end
+
+    # set the session
+    session[:ratings] = @ratings
+
+    # when the param and session variables not match
+    if params[:sort_by] != session[:sort_by] || params[:ratings] != session[:ratings]
+      flash.keep
+      redirect_to :sort_by => sorting_by, :ratings => @ratings
+    end
+    
+    # fliter the movies based on the checkbox selected and sort it by the given type
+    # order_by will be nil for the first time but it will work just fine
+    @movies = Movie.where("rating in (?)", @ratings.keys).order(order_by)
+
   end
 
   def new
-    # default: render 'new' template
+    # default: render 'new' template`
   end
 
   def create
@@ -43,3 +81,4 @@ class MoviesController < ApplicationController
   end
 
 end
+
